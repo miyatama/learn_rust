9b3be8e69d854a868c8d7570a59f449acbce38a9
miyatama <n.miyata080825@gmail.com>
2024-06-26 18:34:09 +0900

    交点計算の参考コードを追記

--- algorithm/09_計算幾何学/voronoi/src/logics/beach_line.rs
+++ algorithm/09_計算幾何学/voronoi/src/logics/beach_line.rs
@@ -27,6 +27,9 @@ impl Arc {
         (sub_line - self.focal_point.y) / 2.0
     }
 
+    /**
+     * 放物線の交差を計算
+     *  */
     pub fn get_cross_points(&self, b: Arc, base_line: f64) -> f64 {
         let p = self.get_v(base_line);
         // v = (h, k)
@@ -37,9 +40,51 @@ impl Arc {
         // より
         // y = (x^2 - 2xh1 + h1^2 + 4p1k1) / 4p1
         // y = (x^2 - 2xh2 + h2^2 + 4p2k2) / 4p2
-        // 
+        //
         // 焦点のy位置が同じ場合はx距離の半分が交差
         // 焦点のy位置が異なる場合は別途計算
+
+        // 参考コード
+        /**
+        // 左から見て母点index1の放物線と母点index2の放物線が交わる交点の位置を計算する関数
+        PVector getIntersection (
+          ArrayList<PVector> generating_points,
+          int index1, // 左の母点
+          int index2, // 右の母点
+          float rho // 準線の位置
+        ){
+          PVector intersect = new PVector(0.0,0.0);
+
+          float x1 = (float) generating_points.get(index1).x;
+          float y1 = (float) generating_points.get(index1).y;
+          float x2 = (float) generating_points.get(index2).x;
+          float y2 = (float) generating_points.get(index2).y;
+
+          float a = y2 - y1;
+          float b = (rho-y1)*x2-(rho-y2)*x1;
+          float c = (rho-y1)*pow(x2,2)-(rho-y2)*pow(x1,2)+(y1-y2)*(rho-y1)*(rho-y2);
+
+          if( abs(y1 - rho) < 0.001 ){
+            intersect.x = x1;
+            intersect.y = quadratic_func(intersect.x, x2, y2, rho);
+          } else if( abs(y2 - rho) < 0.001 ){
+            intersect.x = x2;
+            intersect.y = quadratic_func(intersect.x, x1, y1, rho);
+          } else if( abs(a) < 0.001 ){
+            intersect.x = c/b/2.0;
+            intersect.y = quadratic_func(intersect.x, x1, y1, rho);
+          } else if ( index1 < index2 ){
+            intersect.x = (b-sqrt(pow(b,2)-a*c))/a;
+            intersect.y = quadratic_func(intersect.x, x1, y1, rho);
+          } else {
+            intersect.x = (b-sqrt(pow(b,2)-a*c))/a;
+            intersect.y = quadratic_func(intersect.x, x2, y2, rho);
+          }
+
+          return intersect;
+
+        }
+                 */
         0.0
     }
 
@@ -92,7 +137,9 @@ pub fn create_beach_line(base_y: f64, points: &Vec<Point>, width: f64) -> BeachL
         .fold(f64::MIN, |m, v| m.max(v));
     for i in 0..points.len() {
         let point = points[i].clone();
-        let arc = Arc { focal_point: point.clone() };
+        let arc = Arc {
+            focal_point: point.clone(),
+        };
         let (min_x, max_x) = arc.get_x_range(0.0, base_y, width, max_y);
         let v = arc.get_v(base_y);
         let mut assign = true;
@@ -117,10 +164,8 @@ pub fn create_beach_line(base_y: f64, points: &Vec<Point>, width: f64) -> BeachL
         }
         // 交点を持つ要素を抽出
         let mut cross_ids: Vec<usize> = Vec::new();
-        for j in 0..factors.len() {
-
-        }
-            factors.push(BeachLineFactor::Arc(point.id));
+        for j in 0..factors.len() {}
+        factors.push(BeachLineFactor::Arc(point.id));
     }
     let arcs = points
         .iter()
