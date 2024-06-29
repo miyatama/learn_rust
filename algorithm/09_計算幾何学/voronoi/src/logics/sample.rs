6b5f3c39445d3b03a9c8d1749cf530fef8fcecb8
miyatama <n.miyata080825@gmail.com>
2024-06-29 18:26:12 +0900

    Site, Circleイベントの組み込み

--- algorithm/09_計算幾何学/voronoi/src/logics/sample.rs
+++ algorithm/09_計算幾何学/voronoi/src/logics/sample.rs
@@ -43,6 +43,162 @@ fn draw_voronoi_diagram(height: f64, width: f64, points: &Vec<Point>) {
         site_event_timing.push(points[i].y);
     }
     site_event_timing.push(last_event_timing);
+
+    let mut site_event_number = 2;
+    let mut present_event_timing = site_event_timing.get(site_event_number);
+    let mut previous_event_timing = site_event_timing.get(site_event_number - 1);
+    let mut event_type = 0;
+    let mut intersection_num = 0;
+
+    let mut next_generating_point_index = 2;
+    let mut start: Point = Point { x: 0.0, y: 0.0 };
+    let mut end: Point = Point { x: 0.0, y: 0.0 };
+    let mut intersection_pos: Vec<Point> = Vec::new();
+    let mut next_circle_event_timing = last_event_timing;
+    let mut exception_index: Vec<usize> = Vec::new();
+
+    while present_event_timing < last_event_timing {
+        println!("event timing: {}", present_event_timing);
+        let intersection_num = intersections.size() / 2;
+
+        // intersection_pos = new PVector[intersection_num];
+        let intersection_pos = Point { x: 0.0, y: 0.0 };
+        for i in 0..intersection_num {
+            let index1 = intersections.get(2 * i);
+            let index2 = intersections.get(2 * i + 1);
+            let start = getIntersection(generating_points, index1, index2, previous_event_timing);
+            let end = getIntersection(generating_points, index1, index2, present_event_timing);
+            // line(start.x, start.y, end.x, end.y);
+
+            intersection_pos[i] = end.copy(); // 交点位置の更新のため、交点位置を保持しておく
+        }
+
+        // intersectionsの更新
+        match event_type {
+            0 => {
+                println!("site event");
+                let mut medial_insert_flag = 0;
+                let mut insert_position = intersection_num;
+                for i in 0..intersection_num {
+                    if (generating_points.get(next_generating_point_index).x
+                        - intersection_pos[i].x)
+                        .abs()
+                        < 0.001
+                    {
+                        insert_position = i;
+                        medial_insert_flag = 1;
+                        exception_index.push(next_generating_point_index);
+                        break;
+                    }
+                    if generating_points.get(next_generating_point_index).x < intersection_pos[j].x
+                    {
+                        insert_position = i;
+                        break;
+                    }
+                }
+                // 新たな交点の位置を挿入
+                if medial_insert_flag == 1 {
+                    // 新しく追加される母点のx座標がある交点の位置のx座標が一致する場合
+                    intersections.push(2 * insert_position + 1, next_generating_point_index);
+                    intersections.push(2 * (insert_position + 1), next_generating_point_index);
+                } else if insert_position == 0 {
+                    // 挿入位置が一番左側の場合
+                    let outer_index = intersections.get(0);
+                    intersections.push(0, outer_index);
+                    intersections.push(1, next_generating_point_index);
+                    intersections.push(2, next_generating_point_index);
+                    intersections.push(3, outer_index);
+                } else if insert_position == intersection_num {
+                    // 挿入位置が一番右側の場合
+                    let inner_index = intersections.get(2 * (insert_position - 1) + 1);
+                    intersections.push(inner_index);
+                    intersections.push(next_generating_point_index);
+                    intersections.push(next_generating_point_index);
+                    intersections.push(inner_index);
+                } else {
+                    // 挿入位置が配列の位置の間
+                    let inner_index = intersections.get(2 * (insert_position - 1) + 1);
+                    let outer_index = intersections.get(2 * insert_position);
+                    intersections.push(2 * insert_position, inner_index);
+                    intersections.push(2 * insert_position + 1, next_generating_point_index);
+                    intersections.push(2 * (insert_position + 1), next_generating_point_index);
+                    intersections.push(2 * (insert_position + 1) + 1, outer_index);
+                }
+                // 次の母点にindexを更新
+                next_generating_point_index += 1;
+                // 汀線上の交点の数を更新
+                intersection_num += 1;
+            }
+            _ => {
+                println!("circle event");
+                // 隣り合う交点の位置が重なった場合、交点同士を合体させる
+                let mut remove_index: Vec<usize> = Vec::new();
+                for i in 1..intersection_num {
+                    if exception_index.indexOf(intersections.get(2 * i)) == -1
+                        && intersections.get(2 * j + 1) != intersections.get(2 * (i - 1))
+                        && intersection_pos[j].dist(intersection_pos[i - 1]) < 2.0
+                    {
+                        remove_index.push(i);
+                    }
+                }
+                println!("除外点:", remove_index);
+                let remove_num = remove_index.size();
+                for i in (0..(remove_num - 1)).rev() {
+                    intersections.remove(2 * remove_index.get(i));
+                    intersections.remove(2 * (remove_index.get(i) - 1) + 1);
+                    intersection_num -= 1;
+                }
+            }
+        }
+        println!("calculate next circle event timing");
+        /*
+
+        next_circle_event_timing = last_event_timing;
+        for(int j=1; j<intersection_num; j++){
+          // 3つの母点のindexを取得
+          int[] circle_point_index = new int[3];
+          circle_point_index[0] = intersections.get(2*(j-1));
+          circle_point_index[1] = intersections.get(2*j);
+          circle_point_index[2] = intersections.get(2*j+1);
+          // 選択された隣り合った2つの交点のindexが異なる場合計算する
+          if( circle_point_index[0] != circle_point_index[2] ){
+            // 選択された3点を通る円の中心と半径を算出する
+            PVector circle_center;
+            float circle_radius;
+            // 関数を挿入
+            PVector[] circle_point = new PVector[3];
+            for(int k=0; k<3; k++){
+              circle_point[k] = generating_points.get(circle_point_index[k]).copy();
+            }
+            circle_center = getCercleCenter(circle_point);
+            circle_radius = circle_point[0].dist(circle_center.copy());
+            // 「円の中心のy座標＋半径」の値が現状のイベントタイミング以上かつ次のサークルイベント発生タイミングより小さい値であれば、
+            // 次のサークルイベント発生タイミングを「円の中心のy座標＋半径」の値に更新する
+            if( event_type == 0 && exception_index.indexOf(circle_point_index[1]) == -1 && (circle_center.y + circle_radius) > present_event_timing - 0.001 && circle_center.y + circle_radius <= next_circle_event_timing ){
+              next_circle_event_timing = circle_center.y + circle_radius;
+            } else if( event_type == 1 && present_event_timing < circle_center.y + circle_radius && circle_center.y + circle_radius < next_circle_event_timing ){
+              next_circle_event_timing = circle_center.y + circle_radius;
+            } else {
+            }
+          }
+        }
+             */
+
+        println!("calculate present event timing");
+        /*
+        previous_event_timing = present_event_timing;
+        if( next_circle_event_timing >= site_event_timing.get(next_generating_point_index) ){
+          present_event_timing = site_event_timing.get(next_generating_point_index);
+          event_type = 0;
+        } else {
+          present_event_timing = next_circle_event_timing;
+          event_type = 1;
+        }
+        if( abs( present_event_timing - previous_event_timing ) > 0.001 ){
+          exception_index.clear();
+        }
+              */
+    }
 }
 
 /**
