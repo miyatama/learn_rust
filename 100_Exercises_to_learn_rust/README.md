eefffe860e92ecfe9e04ef0915bd31dab894d893
miyatama <n.miyata080825@gmail.com>
2024-08-13 08:02:14 +0900

    excercise panic

---
+++ 100_Exercises_to_learn_rust/README.md
@@ -0,0 +1,3 @@
+# outline
+
+[100 Exercises To Learn Rust](https://rust-exercises.com/100-exercises/)
---
+++ 100_Exercises_to_learn_rust/excercise_02_04_panics/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = excercise_02_04_panics::add(5, 2);
+    println!("result is {}", result);
+}
