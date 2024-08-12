30659b53ff6664654819a41192b8ca3bb16be166
miyatama <n.miyata080825@gmail.com>
2024-08-13 08:13:17 +0900

    excrcise 02.05 factorial

---
+++ 100_Exercises_to_learn_rust/excrcise_02_05_factorial/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn factorial(value: u64) -> u64 {
+    if value <= 0 {
+        return 1;
+    }
+    factorial(value - 1) * value
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+}
---
+++ 100_Exercises_to_learn_rust/excrcise_02_05_factorial/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = excrcise_02_05_factorial::factorial(3);
+    println!("result: {}", result);
+}
