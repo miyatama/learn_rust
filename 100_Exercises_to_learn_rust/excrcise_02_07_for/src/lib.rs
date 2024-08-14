03a2159ac99e9ecd693e8e941f6756442134332a
miyatama <n.miyata080825@gmail.com>
2024-08-14 13:57:12 +0900

    excrcise 02.07

---
+++ 100_Exercises_to_learn_rust/excrcise_02_07_for/src/lib.rs
@@ -0,0 +1,33 @@
+// Rewrite the factorial function using a `for` loop.
+pub fn factorial(n: u32) -> u32 {
+    let mut sum = 1;
+    for i in 1..(n + 1) {
+        sum = sum * i;
+    }
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+}
---
+++ 100_Exercises_to_learn_rust/excrcise_02_07_for/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = excrcise_02_07_for::factorial(3);
+    println!("result is {}", result);
+}
