8809be17781adee87dc99d6d6177fbc34810ce7a
miyatama <n.miyata080825@gmail.com>
2024-08-16 08:23:49 +0900

    excrcise 02.10

---
+++ 100_Exercises_to_learn_rust/excrcise_02_09_saturating/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = excrcise_02_09_saturating::factorial();
+    println!("result is {}", reuslt);
+}
---
+++ 100_Exercises_to_learn_rust/excrsise_02_10_casting/src/lib.rs
@@ -0,0 +1,41 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn under_score_test() {
+        let a: u32 = 10;
+        let b: u64 = a as u64;
+        let c: u64 = a as _;
+        assert_eq!(b, c);
+    }
+
+    #[test]
+    fn u16_to_u32() {
+        let v: u32 = 47;
+        assert_eq!(47u16 as u32, v);
+    }
+
+    #[test]
+    fn u8_to_i8() {
+        // The compiler is smart enough to know that the value 255 cannot fit
+        // inside an i8, so it'll emit a hard error. We intentionally disable
+        // this guardrail to make this (bad) conversion possible.
+        // The compiler is only able to pick on this because the value is a
+        // literal. If we were to use a variable, the compiler wouldn't be able to
+        // catch this at compile time.
+        #[allow(overflowing_literals)]
+        let x = { 255 as i8 };
+
+        // You could solve this by using exactly the same expression as above,
+        // but that would defeat the purpose of the exercise. Instead, use a genuine
+        // `i8` value that is equivalent to `255` when converted from `u8`.
+        let y: i8 = x as _;
+
+        assert_eq!(x, y);
+    }
+
+    #[test]
+    fn bool_to_u8() {
+        let v: u8 = true as u8;
+        assert_eq!(true as u8, v);
+    }
+}
