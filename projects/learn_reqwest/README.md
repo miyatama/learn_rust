ad5bed51f045da4f3a9fb0ef14dc8c151027563a
miyatama <n.miyata080825@gmail.com>
2024-10-30 18:32:17 +0900

    reqwest呼び出しサンプル

---
+++ projects/learn_reqwest/README.md
@@ -0,0 +1,5 @@
+test
+
+```shell
+cargo run -- "https://raw.githubusercontent.com/yavuzceliker/sample-images/refs/heads/main/images/image-1.jpg" image-1.jpg
+```
---
+++ projects/learn_reqwest/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::env;
+use std::fs::File;
+use bytes::Buf;
+use reqwest;
+
+#[tokio::main]
+async fn main() {
+    let args : Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("argument not enough");
+        return
+    }
+    let url = args[1].as_str();
+    let outfile = args[2].as_str();
+    match run(&url, &outfile).await {
+        Ok(status_code) => println!("download success! status is {}", status_code),
+        Err(err) => eprintln!("download failed.{:?}", err),
+    }
+}
+
+async fn run(url: &str, outfile: &str) -> Result<u16, Box<dyn std::error::Error>> {
+    let res = reqwest::get(url).await?;
+    let status_code = res.status().as_u16();
+    let mut r = res.bytes().await?.reader();
+    let mut w = File::create(outfile)?;
+    io::copy(&mut r, &mut w);
+    Ok(status_code)
+}
