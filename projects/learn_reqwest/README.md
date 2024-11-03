4b2583a5430d9589bab237594c31118b5c4f1abc
miyatama <n.miyata080825@gmail.com>
2024-11-03 16:22:54 +0900

    一括ダウンロード(async await join_all)

--- projects/learn_reqwest/README.md
+++ projects/learn_reqwest/README.md
@@ -8,3 +8,11 @@ web_app_todoを起動
 ```shell
 cargo run -- "https://raw.githubusercontent.com/yavuzceliker/sample-images/refs/heads/main/images/image-1.jpg" image-1.jpg
 ```
+
+# memo
+
++ [sample-images](https://github.com/yavuzceliker/sample-images)
++ [【Rust】Future, Resultの処理をチェーンさせる際のチートシート](https://blog.oskn259.com/article/rust_future_result)
++ [Rust で借用 (参照) を取り扱うときの大事な心構え](https://blog.mudatobunka.org/entry/2023/09/29/120000)
++ [ライフタイムで参照を検証する](https://doc.rust-jp.rs/book-ja/ch10-03-lifetime-syntax.html)
++ [How to use “await?” inside vec.iter().map() in an async fn?](https://users.rust-lang.org/t/how-to-use-await-inside-vec-iter-map-in-an-async-fn/65416)
--- projects/learn_reqwest/src/main.rs
+++ projects/learn_reqwest/src/main.rs
@@ -1,4 +1,6 @@
 use bytes::Buf;
+use futures::future;
+use futures::future::join_all;
 use reqwest;
 use reqwest::header::CONTENT_TYPE;
 use serde::{Deserialize, Serialize};
@@ -20,6 +22,11 @@ async fn main() {
         Err(err) => eprintln!("download failed.{:?}", err),
     }
 
+    match run_get_pictures().await {
+        Ok(_) => println!("download pictures success!"),
+        Err(err) => eprintln!("download pictures failed.{:?}", err),
+    }
+
     let post_url = "http://localhost:8080/update_memo";
     let post_result_file = "post_result.json";
     match run_post(&post_url, &post_result_file).await {
@@ -28,7 +35,10 @@ async fn main() {
     }
 }
 
-async fn run_get_picture(url: &str, outfile: &str) -> Result<u16, Box<dyn std::error::Error>> {
+async fn run_get_picture<'a>(
+    url: &'a str,
+    outfile: &'a str,
+) -> Result<u16, Box<dyn std::error::Error>> {
     let res = reqwest::get(url).await?;
     let status_code = res.status().as_u16();
     let mut r = res.bytes().await?.reader();
@@ -50,8 +60,33 @@ async fn run_post(url: &str, outfile: &str) -> Result<u16, Box<dyn std::error::E
         .send()
         .await?;
     let status_code = res.status().as_u16();
-    let mut content = res.text().await?;
+    let content = res.text().await?;
     let mut w = File::create(outfile)?;
     w.write_all(content.as_bytes());
     Ok(status_code)
+}
+
+async fn run_get_pictures() -> Result<(), Box<dyn std::error::Error>> {
+    /* use macro_rules format!
+    macro_rules! image_filename_base {
+        () => {
+            "image-{}.jpg"
+        };
+    }
+     */
+    let download_infos = (10..20)
+        .map(|index| {
+            (
+                format!("https://raw.githubusercontent.com/yavuzceliker/sample-images/refs/heads/main/images/image-{}.jpg", &index),
+                format!("image-{}.jpg", &index),
+            )
+        })
+        .collect::<Vec<(String, String)>>();
+
+    let download_pictures = download_infos
+        .iter()
+        .map(|(url, image_name)| run_get_picture(&url, &image_name))
+        .collect::<Vec<_>>();
+    join_all(download_pictures).await;
+    Ok(())
 }
