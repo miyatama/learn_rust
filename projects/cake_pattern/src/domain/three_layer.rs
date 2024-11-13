f232c4cc67c24ffbd250c8470a136eebfc399a17
miyatama <n.miyata080825@gmail.com>
2024-11-14 08:32:41 +0900

    三層のテスト追加

--- projects/cake_pattern/src/domain/three_layer.rs
+++ projects/cake_pattern/src/domain/three_layer.rs
@@ -1,29 +1,29 @@
-pub trait IsSvcA {
+pub trait UsesSvcA {
     fn a(&self) -> String;
 }
 
 pub trait SvcA {}
 
-impl<T: SvcA> IsSvcA for T {
+impl<T: SvcA> UsesSvcA for T {
     fn a(&self) -> String {
         "svc-a".to_owned()
     }
 }
 
 // Provide A service.
-pub trait HaveSvcA {
-    type A: IsSvcA; // Not SvcA
+pub trait ProvidesSvcA {
+    type A: UsesSvcA; // Not SvcA
     fn get_svc_a(&self) -> &Self::A;
 }
 
-pub trait IsSvcB {
+pub trait UsesSvcB {
     fn b(&self) -> String;
 }
 
-// SvcB depends on HaveSvcA instead of IsSvcA.
-pub trait SvcB: HaveSvcA {}
+// SvcB depends on ProvidesSvcA instead of UsesSvcA.
+pub trait SvcB: ProvidesSvcA {}
 
-impl<T: SvcB> IsSvcB for T {
+impl<T: SvcB> UsesSvcB for T {
     fn b(&self) -> String {
         let a = self.get_svc_a();
         format!("a: {}, b: {}", a.a(), "svc-b")
@@ -31,12 +31,12 @@ impl<T: SvcB> IsSvcB for T {
 }
 
 // Provide B service.
-pub trait HaveSvcB {
-    type B: IsSvcB; // Not SvcB
-    fn get_svc_b(&self) -> &Self::B;
+pub trait ProvidesSvcB {
+    type B: UsesSvcB; // Not SvcB
+    fn get_svc_b(&self) -> Self::B;
 }
 
-pub fn use_b<S: HaveSvcB>(svc: S) -> String {
+pub fn use_b<S: ProvidesSvcB>(svc: S) -> String {
     let b = svc.get_svc_b();
     format!("[use] {}", b.b())
 }
@@ -44,4 +44,26 @@ pub fn use_b<S: HaveSvcB>(svc: S) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_use_b() {
+        struct MockUses {};
+        impl UsesSvcB for MockUses {
+            fn b(&self) -> String {
+                "test".to_string()
+            }
+        }
+
+        struct MockProvides {};
+        impl ProvidesSvcB for MockProvides {
+            type B = MockUses;
+            fn get_svc_b(&self) -> Self::B {
+                MockUses {}
+            }
+        }
+        let mock = MockProvides {};
+        let actual = use_b(mock);
+        let expect = "[use] test".to_string();
+        assert_eq!(actual, expect);
+    }
 }
