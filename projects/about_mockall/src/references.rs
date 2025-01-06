3ab58b59a4450bd3b26e33f29a5030aaae829b0b
miyatama <n.miyata080825@gmail.com>
2025-01-06 19:14:00 +0900

    mockallのreference残り分

--- projects/about_mockall/src/references.rs
+++ projects/about_mockall/src/references.rs
@@ -1,5 +1,6 @@
 use mockall::predicate::*;
 use mockall::*;
+use std::fmt::Display;
 
 #[automock]
 trait MyTrait {
@@ -11,6 +12,14 @@ struct Thing(u32);
 #[automock]
 trait Container {
     fn get(&self, i: u32) -> &'static Thing;
+    fn get2(&mut self, i: u32) -> &Thing;
+    fn get3(&mut self, i: u32) -> &mut Thing;
+}
+
+#[automock]
+trait Foo {
+    fn name(&self) -> &str;
+    fn name2(&self) -> &dyn Display;
 }
 
 pub fn call_references() {
@@ -23,14 +32,24 @@ pub fn call_references() {
     let mock = MyStruct {};
     let _value = func01(&mock);
     const THING: Thing = Thing(200);
-    struct MyContainer {}
+    struct MyContainer {
+        thing: Thing,
+    }
     impl Container for MyContainer {
         fn get(&self, i: u32) -> &'static Thing {
             &THING
         }
+        fn get2(&mut self, i: u32) -> &Thing {
+            self.thing = Thing(i);
+            &self.thing
+        }
+        fn get3(&mut self, i: u32) -> &mut Thing {
+            &mut self.thing
+        }
     }
-    let mut mock = MyContainer {};
+    let mut mock = MyContainer { thing: Thing(10) };
     func02(&mock);
+    func03(&mut mock);
 }
 
 fn func01(mock: &dyn MyTrait) -> u32 {
@@ -40,6 +59,10 @@ fn func01(mock: &dyn MyTrait) -> u32 {
 
 fn func02(mock: &dyn Container) -> u32 {
     mock.get(500).0
+}
+
+fn func03(mock: &mut dyn Container) -> u32 {
+    mock.get2(500).0
 }
 
 #[cfg(test)]
@@ -65,5 +88,38 @@ mod tests {
         let expect = 100;
         let actual = func02(&mock);
         assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn test03() {
+        let mut mock = MockContainer::new();
+        let thing: Thing = Thing(100);
+        mock.expect_get2().return_const(thing);
+        let expect = 100;
+        let actual = func03(&mut mock);
+        assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn test04() {
+        let mut mock = MockContainer::new();
+        let thing: Thing = Thing(100);
+        mock.expect_get3().return_var(thing);
+        mock.get3(0).0 = 50;
+        assert_eq!(50, mock.get3(0).0);
+    }
+
+    #[test]
+    fn test05() {
+        let mut mock = MockFoo::new();
+        mock.expect_name().return_const("abcd".to_owned());
+        assert_eq!("abcd", mock.name());
+    }
+
+    #[test]
+    fn test06() {
+        let mut mock = MockFoo::new();
+        mock.expect_name2().return_const(Box::new("abcd"));
+        assert_eq!("abcd", format!("{}", mock.name2()));
     }
 }
