b738c3758506853da63073733f4ea34622a0ba84
miyatama <n.miyata080825@gmail.com>
2025-01-10 14:18:05 +0900

    mockallのgenericsその2

--- projects/about_mockall/src/generics.rs
+++ projects/about_mockall/src/generics.rs
@@ -1,9 +1,21 @@
 use mockall::predicate::*;
 use mockall::*;
 
+struct X<'a>(&'a i32);
+
 #[automock]
 trait Foo {
     fn foo<T: 'static>(&self, t: T) -> i32;
+}
+
+#[automock]
+trait Bar {
+    fn bar<'a>(&self, x: X<'a>) -> i32;
+}
+
+#[automock]
+trait Huu<T> {
+    fn huu(&self, t: T) -> i32;
 }
 
 pub fn call_generics() {
@@ -16,6 +28,26 @@ pub fn call_generics() {
     let foo = FooStruct {};
     let _value = foo.foo(5i16);
     let _value = foo.foo(5i8);
+
+    struct BarStruct {}
+    impl Bar for BarStruct {
+        fn bar<'a>(&self, x: X<'a>) -> i32 {
+            *x.0
+        }
+    }
+    let bar = BarStruct {};
+    let x_value = 8i32;
+    let x = X(&x_value);
+    let _value = bar.bar(x);
+
+    struct HuuStruct {}
+    impl<T> Huu<T> for HuuStruct {
+        fn huu(&self, _t: T) -> i32 {
+            8i32
+        }
+    }
+    let huu = HuuStruct {};
+    let _value = huu.huu(8u32);
 }
 
 #[cfg(test)]
@@ -34,5 +66,20 @@ mod tests {
         let expect = -5i32;
         let actual = mock.foo(5i8);
         assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn test02() {
+        let mut mock = MockBar::new();
+        mock.expect_bar().withf(|f| *f.0 == 5).return_const(101);
+        let x = X(&5);
+        assert_eq!(101, mock.bar(x));
+    }
+
+    #[test]
+    fn test03() {
+        let mut mock = MockHuu::new();
+        mock.expect_huu().returning(|_t| 101i32);
+        assert_eq!(101, mock.huu(300));
     }
 }
