f933373dc6c03d6bf0e5d705fae81e38944e2c51
miyatama <n.miyata080825@gmail.com>
2025-01-23 13:29:45 +0900

    StarWarsのExampleはレベル高いので全消し

--- projects/web_app_todo_graphql/src/main.rs
+++ projects/web_app_todo_graphql/src/main.rs
@@ -1,5 +1,5 @@
 use async_graphql::{
-    http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Request, Schema,
+    http::GraphiQLSource, EmptyMutation, EmptySubscription, 
 };
 use axum::{
     extract::Extension,
@@ -7,12 +7,10 @@ use axum::{
     routing::get,
     Json, Router,
 };
-use std::error::Error;
 
-mod models;
-pub type Todo = Schema<models::QueryRoot, EmptyMutation, EmptySubscription>;
+pub type Todo = async_graphql::Schema<Query, EmptyMutation, EmptySubscription>;
 
-async fn graphql_handler(schema: Extension<Todo>, req: Json<Request>) -> Json<Response> {
+async fn graphql_handler(schema: Extension<Todo>, req: Json<async_graphql::Request>) -> Json<async_graphql::Response> {
     schema.execute(req.0).await.into()
 }
 
@@ -22,8 +20,17 @@ async fn graphql() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    let schema = async_graphql::Schema::build(Query, EmptyMutation, EmptySubscription).finish();
     let app = Router::new().route("/", get(graphql).post(graphql_handler));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+
+struct Query;
+#[async_graphql::Object]
+impl Query {
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
 }
--- projects/web_app_todo_graphql/src/models.rs
+++
@@ -1,7 +0,0 @@
-pub mod episode;
-pub mod query_root;
-pub mod starwars_char;
-pub mod starwars;
-pub mod human;
-pub mod droid;
-pub mod character;
