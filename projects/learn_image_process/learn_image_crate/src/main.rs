92124e85acdfbea07dde2c1609ffc5aa04ad8c7e
miyatama <n.miyata080825@gmail.com>
2025-02-24 12:50:56 +0900

    Decode & Encode

--- projects/learn_image_process/learn_image_crate/src/main.rs
+++ projects/learn_image_process/learn_image_crate/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     log::set_max_level(LevelFilter::Debug);
     read_and_save();
     create_bytes();
+    decoder_encoder();
 }
 
 fn read_and_save() {
@@ -57,5 +58,49 @@ fn create_bytes() {
             log::error!("{:?}", e);
             return;
         }
+    }
+}
+
+use image::ImageDecoder;
+fn decoder_encoder() {
+    let file = match std::fs::File::open("cargo.png") {
+        Ok(file) => file,
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+    };
+    let mut reader = std::io::BufReader::new(file);
+    let mut decoder = match image::codecs::png::PngDecoder::new(&mut reader) {
+        Ok(decoder) => decoder,
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+    };
+    let icc = decoder.icc_profile();
+    log::info!("icc: {:?}", icc);
+    let img = match image::DynamicImage::from_decoder(decoder) {
+        Ok(img) => img,
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+    };
+
+    let mut writer = match std::fs::File::create("cargo3.png") {
+        Ok(file) => file,
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+    };
+    let encoder = image::codecs::png::PngEncoder::new(&mut writer);
+    match img.write_with_encoder(encoder) {
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+        _ => {}
     }
 }
