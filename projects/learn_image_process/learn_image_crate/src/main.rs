80be3c24ecd0112738e36829bb209350e352f151
miyatama <n.miyata080825@gmail.com>
2025-02-24 12:08:11 +0900

    バイト列への変換

--- projects/learn_image_process/learn_image_crate/src/main.rs
+++ projects/learn_image_process/learn_image_crate/src/main.rs
@@ -1,7 +1,7 @@
 mod util;
 use self::util::app_logger::AppLogger;
 use image::ImageReader;
-use log::{ LevelFilter};
+use log::LevelFilter;
 
 static LOGGER: AppLogger = AppLogger {};
 
@@ -9,6 +9,7 @@ fn main() {
     log::set_logger(&LOGGER).unwrap();
     log::set_max_level(LevelFilter::Debug);
     read_and_save();
+    create_bytes();
 }
 
 fn read_and_save() {
@@ -27,4 +28,34 @@ fn read_and_save() {
         }
     };
     let _ = img.save("cargo2.png");
+}
+
+fn create_bytes() {
+    let reader = match ImageReader::open("cargo.png") {
+        Ok(reader) => reader,
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+    };
+    let img = match reader.decode() {
+        Ok(image) => image,
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+    };
+    let mut bytes: Vec<u8> = Vec::new();
+    match img.write_to(
+        &mut std::io::Cursor::new(&mut bytes),
+        image::ImageFormat::Png,
+    ) {
+        Ok(_) => {
+            log::info!("bytes: {:?}", bytes);
+        }
+        Err(e) => {
+            log::error!("{:?}", e);
+            return;
+        }
+    }
 }
