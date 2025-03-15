dd0f968a35b4e57483371052be7ea5e11a503019
miyatama <n.miyata080825@gmail.com>
2025-03-15 17:14:40 +0900

    gaussian_blur_f32呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/filter.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/filter.rs
@@ -7,11 +7,12 @@ pub fn run() {
     let img_gray = img.to_luma8();
     let img_result = imageproc::filter::bilateral_filter(&img_gray, 10, 0.5, 0.2);
     img_result
-        .save("bilateral_filter_result.png")
+        .save("filter_bilateral_filter.png")
         .expect("failed to save bilateral_filter image");
+
     let img_result = imageproc::filter::box_filter(&img_gray, 10, 10);
     img_result
-        .save("box_filter_result.png")
+        .save("filter_box_filter.png")
         .expect("failed to save box_filter image");
 
     let kernel = [
@@ -29,42 +30,40 @@ pub fn run() {
     // type: image::buffer_::ImageBuffer<image::color::Rgb<f32>, alloc::vec::Vec<f32>>
     let filter_result =
         imageproc::filter::filter3x3::<image::Rgb<f32>, f32, f32>(&image_buffer, &kernel);
-    log::debug!(
-        "filter result type: {}",
-        get_type_name(filter_result.clone())
-    );
     let filter_result = image::DynamicImage::ImageRgb32F(filter_result);
-    log::debug!(
-        "parsed filter result type: {}",
-        get_type_name(filter_result.clone())
-    );
     filter_result
         .into_rgb8()
-        .save("filter3x3_result.png")
+        .save("filter_filter3x3.png")
         .expect("failed to save filter3x3 image");
 
+    let filter_result = imageproc::filter::gaussian_blur_f32(&image_buffer, 10.5f32);
+    let filter_result = image::DynamicImage::ImageRgb32F(filter_result);
+    filter_result
+        .into_rgb8()
+        .save("filter_gaussian_blur_f32.png")
+        .expect("failed to save gaussian_blur_f32 image");
+
     /*
-    let img_result = gaussian_blur_f32
+    let img_result = imageproc::filter::horizontal_filter
 
-    let img_result = horizontal_filter
+    let img_result = imageproc::filter::laplacian_filter
 
-    let img_result = laplacian_filter
+    let img_result = imageproc::filter::median_filter
 
-    let img_result = median_filter
+    let img_result = imageproc::filter::separable_filter
 
-    let img_result = separable_filter
+    let img_result = imageproc::filter::separable_filter_equal
 
-    let img_result = separable_filter_equal
+    let img_result = imageproc::filter::sharpen3x3
 
-    let img_result = sharpen3x3
+    let img_result = imageproc::filter::sharpen_gaussian
 
-    let img_result = sharpen_gaussian
-
-    let img_result = vertical_filter
+    let img_result = imageproc::filter::vertical_filter
 
              */
 }
 
+#[allow(dead_code)]
 fn get_type_name<T>(_: T) -> String {
     format!("{}", std::any::type_name::<T>())
 }
