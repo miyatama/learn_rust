645203b13733737c1ba290ebd0271672f492dece
miyatama <n.miyata080825@gmail.com>
2025-03-19 12:23:50 +0900

    canny呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/edges.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/edges.rs
@@ -4,4 +4,8 @@ pub fn run() {
 
 fn canny() {
     log::debug!("edges canny");
+    let img = image::open("lena.png").expect("failed to load image");
+    let img_gray = img.clone().to_luma8();
+    let result = imageproc::edges::canny(&img_gray, 30f32, 240f32);
+    result.save("./results/edges_canny.png").unwrap();
 }
