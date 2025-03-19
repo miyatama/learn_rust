abf688de3e58addbd27ccd7820f63d796015386a
miyatama <n.miyata080825@gmail.com>
2025-03-19 11:17:39 +0900

    corners_fast9呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/corners.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/corners.rs
@@ -7,6 +7,23 @@ pub fn run() {
 
 fn corners_fast9() {
     log::debug!("corners corners_fast9");
+    let mut img = image::open("lena.png").expect("failed to load image");
+    let img_gray = img.clone().to_luma8();
+
+    let result = imageproc::corners::corners_fast9(&img_gray, 100);
+    // https://docs.rs/imageproc/0.25.0/imageproc/corners/struct.Corner.html
+
+    let red = image::Rgba([255u8, 0u8, 0u8, 255u8]);
+    result.iter().for_each(|corner| {
+        imageproc::drawing::draw_hollow_circle_mut(
+            &mut img,
+            (corner.x as i32, corner.y as i32),
+            10i32,
+            red,
+        );
+    });
+
+    img.save("./results/corners_corners_fast9.png").unwrap();
 }
 
 fn corners_fast12() {
