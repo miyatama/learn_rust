f39dc4549d851d5a2990fe60ad880005e7d09b43
miyatama <n.miyata080825@gmail.com>
2025-03-19 12:01:01 +0900

    distance_transform呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/distance_transform.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/distance_transform.rs
@@ -6,6 +6,20 @@ pub fn run() {
 
 fn distance_transform() {
     log::debug!("distance_transform");
+    let img = image::open("lena.png").expect("failed to load image");
+    let mut img_gray = img.clone().to_luma8();
+    imageproc::contrast::threshold_mut(
+        &mut img_gray,
+        100u8,
+        imageproc::contrast::ThresholdType::Binary,
+    );
+    // https://docs.rs/imageproc/0.25.0/imageproc/distance_transform/enum.Norm.html
+    let norm = imageproc::distance_transform::Norm::L1;
+    // https://docs.rs/imageproc/0.25.0/imageproc/distance_transform/fn.distance_transform.html
+    let result = imageproc::distance_transform::distance_transform(&img_gray, norm);
+    result 
+        .save("./results/distance_transform_distance_transform.png")
+        .unwrap();
 }
 
 fn distance_transform_mut() {
