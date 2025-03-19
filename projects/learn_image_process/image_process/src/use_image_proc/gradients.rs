3c104b43bc27da9fdd34d0e2732d6051e8bb7dbf
miyatama <n.miyata080825@gmail.com>
2025-03-20 07:21:48 +0900

    horizontal_prewittの呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/gradients.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/gradients.rs
@@ -12,6 +12,14 @@ pub fn run() {
 
 fn horizontal_prewitt() {
     log::debug!("gradients horizontal_prewitt");
+    let img = image::open("lena.png").expect("failed to load image");
+    let img_gray = img.to_luma8();
+
+    let result = imageproc::gradients::horizontal_prewitt(&img_gray);
+    let result = parse_lumai16_to_lumau8(&result);
+    result
+        .save("./results/gradients_horizontal_prewitt.png")
+        .unwrap();
 }
 
 fn horizontal_scharr() {
@@ -44,4 +52,41 @@ fn vertical_scharr() {
 
 fn vertical_sobel() {
     log::debug!("gradients vertical_sobel");
+}
+
+fn parse_lumai16_to_lumau8(
+    img: &imageproc::definitions::Image<image::Luma<i16>>,
+) -> imageproc::definitions::Image<image::Luma<u8>> {
+    let (width, height) = img.dimensions();
+    let pixels = img
+        .pixels()
+        .into_iter()
+        .map(|pixel| pixel.0[0] as f32)
+        .collect::<Vec<f32>>();
+    let min_value = pixels.iter().fold(0.0 / 0.0, |m, v| v.min(m));
+    let max_value = pixels.iter().fold(0.0 / 0.0, |m, v| v.max(m));
+    let limit = min_value.abs().max(max_value.abs());
+    let pixels = pixels
+        .iter()
+        .map(|pixel| {
+            let pixel = *pixel;
+            let sign = if pixel < 0f32 { -1f32 } else { 1f32 };
+            let pixel = pixel * sign;
+            128f32 * (pixel / limit) * sign
+        })
+        .collect::<Vec<f32>>();
+    let correction_value = pixels.iter().fold(0.0 / 0.0, |m, v| v.min(m));
+    let correction_value = if correction_value < 0.0f32 {
+        correction_value.abs()
+    } else {
+        0f32
+    };
+    let pixels = pixels
+        .iter()
+        .map(|value| {
+            let value = *value;
+            (value + correction_value) as u8
+        })
+        .collect::<Vec<u8>>();
+    image::GrayImage::from_raw(width, height, pixels).unwrap()
 }
