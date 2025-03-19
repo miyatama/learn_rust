f1a4a8d04528339a30ee992111ea0ca44de15d35
miyatama <n.miyata080825@gmail.com>
2025-03-19 09:34:16 +0900

    リファクタリング

--- projects/learn_image_process/image_process/src/use_image_proc/contours.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/contours.rs
@@ -2,13 +2,13 @@ pub fn run() {
     log::info!("imageproc contours module");
 
     // TODO 元画像差し替え
-    let mut img = image::open("lena.png").expect("failed to load image");
+    let img = image::open("lena.png").expect("failed to load image");
     // let mut img = image::open("contour_base.png").expect("failed to load image");
     let img_gray = img.clone().to_luma8();
     log::info!("call find_contours");
     let result = imageproc::contours::find_contours::<u8>(&img_gray);
     log::info!("called find_contours");
-    let red = image::Rgba([255u8, 0u8, 0u8, 255u8]);
+    let _red = image::Rgba([255u8, 0u8, 0u8, 255u8]);
     result.iter().for_each(|contour| {
         log::debug!("{:?}", &contour);
     });
