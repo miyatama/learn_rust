880360d068dd4ee702ee169106784a6386bc5ede
miyatama <n.miyata080825@gmail.com>
2025-03-19 11:34:58 +0900

    oriented_fast呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/corners.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/corners.rs
@@ -64,4 +64,21 @@ fn fast_corner_score() {
 
 fn oriented_fast() {
     log::debug!("corners oriented_fast");
+    let mut img = image::open("lena.png").expect("failed to load image");
+    let img_gray = img.clone().to_luma8();
+
+    let result =
+        imageproc::corners::oriented_fast(&img_gray, Some(80), 30usize, 20u32, Some(2000u64));
+    // https://docs.rs/imageproc/0.25.0/imageproc/corners/struct.OrientedFastCorner.html
+    let red = image::Rgba([255u8, 0u8, 0u8, 255u8]);
+    result.iter().for_each(|ofc| {
+        imageproc::drawing::draw_hollow_circle_mut(
+            &mut img,
+            (ofc.corner.x as i32, ofc.corner.y as i32),
+            10i32,
+            red,
+        );
+    });
+
+    img.save("./results/corners_oriented_fast.png").unwrap();
 }
