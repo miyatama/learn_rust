756e89a31a597dbbb9dd4a4417cd04c05c7e1301
miyatama <n.miyata080825@gmail.com>
2025-03-19 11:25:06 +0900

    fast_corner_score呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/corners.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/corners.rs
@@ -28,10 +28,38 @@ fn corners_fast9() {
 
 fn corners_fast12() {
     log::debug!("corners corners_fast12");
+    let mut img = image::open("lena.png").expect("failed to load image");
+    let img_gray = img.clone().to_luma8();
+
+    let result = imageproc::corners::corners_fast12(&img_gray, 100);
+    // https://docs.rs/imageproc/0.25.0/imageproc/corners/struct.Corner.html
+
+    let red = image::Rgba([255u8, 0u8, 0u8, 255u8]);
+    result.iter().for_each(|corner| {
+        imageproc::drawing::draw_hollow_circle_mut(
+            &mut img,
+            (corner.x as i32, corner.y as i32),
+            10i32,
+            red,
+        );
+    });
+
+    img.save("./results/corners_corners_fast12.png").unwrap();
 }
 
 fn fast_corner_score() {
     log::debug!("corners fast_corner_score");
+    let img = image::open("lena.png").expect("failed to load image");
+    let img_gray = img.clone().to_luma8();
+
+    let result = imageproc::corners::fast_corner_score(
+        &img_gray,
+        100,
+        100u32,
+        20u32,
+        imageproc::corners::Fast::Nine,
+    );
+    log::info!("fast_corner_score result: {}", result);
 }
 
 fn oriented_fast() {
