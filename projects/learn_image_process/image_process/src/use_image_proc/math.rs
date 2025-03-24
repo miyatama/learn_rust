713ae40385286c992061076f24119d97238e81a8
miyatama <n.miyata080825@gmail.com>
2025-03-25 07:58:00 +0900

    mathモジュールの関数群呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/math.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/math.rs
@@ -1,1 +1,17 @@
-pub fn run() {}
+pub fn run() {
+    l1_norm();
+    l2_norm();
+}
+
+fn l1_norm() {
+    log::debug!("math l1_norm");
+    let value = vec![5f32, 10f32, 25f32];
+    let result = imageproc::math::l1_norm(&value);
+    log::info!("value {:?} l1 norm is {}", value, result);
+}
+fn l2_norm() {
+    log::debug!("math l2_norm");
+    let value = vec![5f32, 10f32, 25f32];
+    let result = imageproc::math::l2_norm(&value);
+    log::info!("value {:?} l2 norm is {}", value, result);
+}
