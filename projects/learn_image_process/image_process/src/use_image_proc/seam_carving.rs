7ee18f5f5c06a0ea1662f9247211288822212d69
miyatama <n.miyata080825@gmail.com>
2025-03-27 07:41:14 +0900

    seam_carvingモジュールの関数群呼び出し

--- projects/learn_image_process/image_process/src/use_image_proc/seam_carving.rs
+++ projects/learn_image_process/image_process/src/use_image_proc/seam_carving.rs
@@ -1,1 +1,28 @@
-pub fn run() {}
+pub fn run() {
+    let img = image::open("lena.png").expect("failed to load image");
+    let image_gray = img.clone().to_luma8();
+    log::debug!("seam_carving find_vertical_seam");
+    let seam = imageproc::seam_carving::find_vertical_seam(&image_gray);
+
+    log::debug!("seam_carving draw_vertical_seams");
+    let seams = vec![seam];
+    let image_result = imageproc::seam_carving::draw_vertical_seams(&image_gray, &seams);
+    image_result
+        .save("./results/seam_carving_draw_vertical_seams.png")
+        .unwrap();
+
+    log::debug!("seam_carving remove_vertical_seam");
+    let seam = imageproc::seam_carving::find_vertical_seam(&image_gray);
+    let image_result = imageproc::seam_carving::remove_vertical_seam(&image_gray, &seam);
+    image_result
+        .save("./results/seam_carving_remove_vertical_seam.png")
+        .unwrap();
+
+    log::debug!("seam_carving shrink_width(skip)");
+    /*
+    let image_result = imageproc::seam_carving::shrink_width(&image_gray, 100u32);
+    image_result
+        .save("./results/seam_carving_shrink_width.png")
+        .unwrap();
+     */
+}
