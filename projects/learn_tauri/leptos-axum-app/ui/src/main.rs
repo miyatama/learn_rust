c390a18aa07d56a904c3af6e81c5d06314cb7694
miyatama <n.miyata080825@gmail.com>
2025-04-30 21:12:11 +0900

    ビルド構成変更

---
+++ projects/learn_tauri/leptos-axum-app/ui/src/main.rs
@@ -0,0 +1,72 @@
+mod todo;
+
+#[cfg(feature = "ssr")]
+use axum::{
+    body::Body,
+    extract::Path,
+    http::Request,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+//Define a handler to test extractor with state
+#[cfg(feature = "ssr")]
+async fn custom_handler(
+    Path(id): Path<String>,
+    req: Request<Body>,
+) -> Response {
+    let handler = leptos_axum::render_app_to_stream_with_context(
+        move || {
+            provide_context(id.clone());
+        },
+        todo::TodoApp,
+    );
+    handler(req).await.into_response()
+}
+
+#[cfg(feature = "ssr")]
+#[tokio::main]
+async fn main() {
+    use crate::todo::{ssr::db, *};
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+
+    simple_logger::init_with_level(log::Level::Error)
+        .expect("couldn't initialize logging");
+
+    let mut conn = db().await.expect("couldn't connect to DB");
+    if let Err(e) = sqlx::migrate!().run(&mut conn).await {
+        eprintln!("{e:?}");
+    }
+
+    // Setting this to None means we'll be using cargo-leptos and its env vars
+    let conf = get_configuration(None).unwrap();
+    let leptos_options = conf.leptos_options;
+    let addr = leptos_options.site_addr;
+    let routes = generate_route_list(TodoApp);
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/special/{id}", get(custom_handler))
+        .leptos_routes(&leptos_options, routes, {
+            let leptos_options = leptos_options.clone();
+            move || shell(leptos_options.clone())
+        })
+        .fallback(leptos_axum::file_and_error_handler(shell))
+        .with_state(leptos_options);
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    println!("listening on http://{}", &addr);
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn main() {
+    use leptos::mount::mount_to_body;
+
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(todo::TodoApp);
+}
